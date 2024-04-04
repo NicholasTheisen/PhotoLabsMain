c065b9aae82a9868b98d55e874a0c567f25f8edb
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-04-03 22:47:11 -0700

    displayed notification

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,13 +5,15 @@ import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
 const TopNavigation = (props) => {
-  const { topics } = props;
+  const { topics, isFavPhotoExist } = props;
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} />
-      <FavBadge />
+      <TopicList
+        topics={topics}
+      />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   );
 };
