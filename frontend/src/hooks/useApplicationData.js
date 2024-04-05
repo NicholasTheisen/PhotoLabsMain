e62d2f4fb21e3f115c5f073a74a837105ecb2cc7
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-04-05 01:00:15 -0700

    implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,32 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useReducer } from "react";
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: "FAV_PHOTO_ADDED",
+  FAV_PHOTO_REMOVED: "FAV_PHOTO_REMOVED",
+  SHOW_MODAL: "SHOW_MODAL",
+  MODAL_PHOTO_DATA: "MODAL_PHOTO_DATA",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  case ACTIONS.FAV_PHOTO_ADDED:
+    return [...state, action.id];
+  case ACTIONS.FAV_PHOTO_REMOVED:
+    return state.filter((id) => id !== action.id);
+  case ACTIONS.SHOW_MODAL:
+    return action.flag;
+  case ACTIONS.MODAL_PHOTO_DATA:
+    return action.item;
+  default:
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    );
+  }
+};
 
 const useApplicationData = () => {
 
-  const [photoIDs, setPhotoIDs] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [modalPhotoData, setModalPhotoData] = useState({});
+  const [photoIDs, dispatch1] = useReducer(reducer, []);
+  const [showModal, dispatch2] = useReducer(reducer, false);
+  const [modalPhotoData, dispatch3] = useReducer(reducer, {});
 
   const updateFavouritedPhotoIDs = (id, action) => {
     if (!action) {
-      setPhotoIDs((prevPhotoIDs) => [...prevPhotoIDs, id]);
+      console.log("add");
+      dispatch1({ type: ACTIONS.FAV_PHOTO_ADDED, id: id });
     } else {
-      setPhotoIDs(oldValues => {
-        return oldValues.filter(itemID => itemID !== id);
-      });
+      console.log("remove");
+      dispatch1({ type: ACTIONS.FAV_PHOTO_REMOVED, id: id });
     }
   };
 
-  const setModalData = (flag, item) => {
-    setShowModal(flag);
-    setModalPhotoData(item);
+  const updateModalData = (flag, item) => {
+    dispatch2({ type: ACTIONS.SHOW_MODAL, flag: flag });
+    dispatch3({ type: ACTIONS.MODAL_PHOTO_DATA, item: item });
   };
+
+  useEffect(() => {
+    console.log(photoIDs);
+  }, [photoIDs]);
 
   return {
     photoIDs,
     showModal,
     modalPhotoData,
     updateFavouritedPhotoIDs,
-    setModalData
+    updateModalData,
   };
 };
 
